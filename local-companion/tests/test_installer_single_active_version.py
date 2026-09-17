d68fc8e9336cdd1aaf176ec3ff6825c50775ab96
Faysk <faysk.nan@gmail.com>
2026-09-17 16:25:37 +0100

    test(companion): align installer contract with rollback-safe MajorUpgrade

--- local-companion/tests/test_installer_single_active_version.py
+++ local-companion/tests/test_installer_single_active_version.py
@@ -18,7 +18,7 @@ def test_major_upgrade_embeds_new_maintenance_helper_before_removing_old_product
     assert 'SourceFile="!(bindpath.App)\\TDACompanionMaintenance.exe"' in source
     assert 'Id="PrepareMajorUpgrade"' in source
     assert 'BinaryRef="TDACompanionMaintenanceUpgradeBinary"' in source
-    assert 'ExeCommand="--prepare-uninstall"' in source
+    assert 'ExeCommand="--prepare-major-upgrade"' in source
     assert '<Custom Action="PrepareMajorUpgrade" Before="RemoveExistingProducts" Condition="WIX_UPGRADE_DETECTED" />' in source
 
 
@@ -29,5 +29,10 @@ def test_process_cleanup_failure_is_never_ignored_by_installer():
     prepare_uninstall = source.split('Id="PrepareUninstall"', 1)[1].split("/>", 1)[0]
     assert 'Return="check"' in prepare_upgrade
     assert 'Return="check"' in prepare_uninstall
-    assert 'Condition=\'REMOVE="ALL"\'' in source
-    assert "NOT UPGRADINGPRODUCTCODE" not in source
+
+
+def test_cached_old_product_uninstall_does_not_duplicate_major_upgrade_preparation():
+    source = _wxs()
+
+    assert 'Condition=\'(REMOVE="ALL") AND (NOT UPGRADINGPRODUCTCODE)\'' in source
+    assert 'Condition="WIX_UPGRADE_DETECTED"' in source
